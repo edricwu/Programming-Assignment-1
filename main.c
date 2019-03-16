ec59ebe0a21355b69fd2e31b14d6cf96651250a8
Edric <edricwu008@gmail.com>
2019-03-17 03:41:54 +0800

    Final from Linux

--- main.c
+++ main.c
@@ -1,11 +1,38 @@
 #include "ProcTreeDPCoinChangeProblem.h"
 
 int main(){
+	clock_t time1, time2;
+	methodType assignment1_method = USEPROCESS; /** USEPROCESS / USESEQUENTIAL / USEPROCESS **/
+	/**
+	int coinSupplyValue[3] = {1,2,3};
+	int coinTypes = 3;
+	int value = 10;
+	getCoinChangeCombination_test(coinSupplyValue, value, coinTypes, assignment1_method);
+	**/
 
-   int coinSupplyValue[3] = {1,2,3};
-   int value = 10;
-   int coinTypes = 3;
-   methodType assignment1_method = USEPROCESS; /** USEPROCESS / USESEQUENTIAL / USEPROCESS **/
-   getCoinChangeCombination_test(coinSupplyValue, value, coinTypes, assignment1_method);
+	//Part6,Qn1
+	int coinSupplyValue[3] = {1,2,3};
+	int coinTypes = 3;
+
+	for (int value = 4; value < 21; value++){
+		time1 = clock();
+		getCoinChangeCombination_test(coinSupplyValue, value, coinTypes, assignment1_method);
+		time2 = clock();
+		printf("%f \n",(double)(time2-time1)/CLOCKS_PER_SEC);
+	}
+
+	/**Part6,Qn2
+	int coinSupplyValue[10] = {1,2,3,4,5,6,7,8,9,10};
+	int coinTypes = 10;
+
+	for (int value = 3; value < 8; value++){
+		time1 = clock();
+		getCoinChangeCombination_test(coinSupplyValue, value, coinTypes, assignment1_method);
+		time2 = clock();
+		printf("%f ",(double)(time2-time1)/CLOCKS_PER_SEC);
+	}**/
+
+
+	
    
 }
